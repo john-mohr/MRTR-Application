087bfaeb8ae1f0f9ce9ac04ec6737111045822d0
jt0sizzler <44917001+jt0sizzler@users.noreply.github.com>
2023-06-17 22:46:03 -0600

    Delete administrative.js
    Clean up Dean mistake

--- administrative.js
+++
@@ -1,6 +0,0 @@
-let sidebar = document.querySelector(".sidebar");
-let sidebarButton = document.querySelector(".sidebarButton");
-
-sidebarButton.onclick = function() {
-    sidebar.classList.toggle("active");
-}
