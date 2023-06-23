653e7427c21d09c61ff94cb9ec706a9c61f50d03
Deanpham1200 <59811472+Deanpham1200@users.noreply.github.com>
2023-06-22 20:18:39 -0600

    Delete mrtr directory

--- mrtr/utils.py
+++
@@ -1,5 +0,0 @@
-from datetime import date, timedelta
-
-# Helper function for calculated transactions
-def prorate(rent, e_date):
-    return (date(e_date.year, e_date.month + 1, 1) - e_date).days * (rent / 30)
