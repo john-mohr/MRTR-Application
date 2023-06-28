ef66be6e4b9a4a93e381b14c9ef78663810aae6b
Deanpham1200 <59811472+Deanpham1200@users.noreply.github.com>
2023-06-27 20:13:58 -0600

    Add files via upload

---
+++ mysite/mrtr/forms.py
@@ -0,0 +1,261 @@
+from django import forms
+from .models import *
+from django.core.validators import MaxValueValidator
+from django.contrib.auth.forms import UserCreationForm
+
+
+class ContactForm(forms.Form):
+    email = forms.EmailField()
+    first_name = forms.CharField(label= 'First Name', max_length = 20)
+    last_name = forms.CharField(label= 'Last Name', max_length = 20)
+    message = message = forms.CharField(widget = forms.Textarea, max_length = 2000)
+
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+
+class BedField(forms.ModelChoiceField):
+    def label_from_instance(self, bed):
+        label = f"{bed.name}"
+        return label
+
+
+class ResidentForm(forms.ModelForm):
+    phone = forms.IntegerField(validators=[MaxValueValidator(9999999999)], required=False)
+    email = forms.EmailField(max_length=62, required=False)
+    door_code = forms.IntegerField(validators=[MaxValueValidator(9999)], required=False)
+    referral_info = forms.CharField(required=False)
+    notes = forms.CharField(widget=forms.Textarea, required=False)
+    occupied_beds = Resident.objects.all().filter(bed_id__isnull=False).distinct()
+    bed = BedField(queryset=Bed.objects.exclude(id__in=occupied_beds.values_list('bed_id', flat=True)))
+    effective_date = forms.DateField(widget=DateInput(), initial=timezone.now)  # for edit_res only
+
+    # Allows selection of resident's current bed and removes rent field for editing
+    def __init__(self, *args, **kwargs):
+        super(ResidentForm, self).__init__(*args, **kwargs)
+        if 'instance' in kwargs:  # edit and readmit
+            res = kwargs.pop('instance')
+            if res.discharge_date is None:  # edit
+                edit_beds = self.occupied_beds.exclude(pk=res.pk)
+                self.fields['bed'].queryset = Bed.objects.exclude(id__in=edit_beds.values_list('bed_id', flat=True))
+            else:  # readmit
+                self.fields['admit_date'].label = 'Readmission date'
+                del self.fields['effective_date']
+        else:  # new
+            del self.fields['effective_date']
+
+    class Meta:
+        model = Resident
+        fields = ['effective_date',  # for edit_res only
+                  'first_name',
+                  'last_name',
+                  'phone',
+                  'email',
+                  'admit_date',
+                  'bed',
+                  'rent',
+                  'door_code',
+                  'referral_info',
+                  'notes',
+                  ]
+        widgets = {
+            'admit_date': DateInput(),
+        }
+
+
+class DischargeResForm(forms.Form):
+    date = forms.DateField(widget=DateInput(), initial=timezone.now)
+    reason = forms.CharField(widget=forms.Textarea, required=False)
+
+
+class ResidentField(forms.ModelChoiceField):
+    def label_from_instance(self, resident):
+        label = resident.first_name + " " + resident.last_name
+        return label
+
+
+class TransactionForm(forms.ModelForm):
+    type = forms.ChoiceField(choices=Transaction.TYPE_CHOICES)
+    resident = ResidentField(queryset=Resident.objects.all())
+    notes = forms.CharField(widget=forms.Textarea, required=False)
+
+    class Meta:
+        model = Transaction
+        fields = ['resident',
+                  'date',
+                  'type',
+                  'amount',
+                  'notes',
+                  ]
+        widgets = {
+            'date': DateInput(),
+        }
+
+
+class RentPaymentForm(forms.ModelForm):
+    method = forms.ChoiceField(choices=Transaction.METHOD_CHOICES, required=False)
+    resident = ResidentField(queryset=Resident.objects.all())
+    notes = forms.CharField(widget=forms.Textarea, required=False)
+
+    class Meta:
+        model = Transaction
+        fields = ['resident',
+                  'date',
+                  'amount',
+                  'method',
+                  'notes',
+                  ]
+        widgets = {
+            'date': DateInput(),
+        }
+
+
+class HouseField(forms.ModelChoiceField):
+    def label_from_instance(self, house):
+        label = f"{house.name}"
+        return label
+
+
+class HouseForm(forms.ModelForm):
+    current_HMs = House.objects.all().filter(manager_id__isnull=False).distinct()
+    manager = ResidentField(
+        queryset=Resident.objects.exclude(id__in=current_HMs.values_list('manager_id', flat=True)), required=False)
+
+    class Meta:
+        model = House
+        fields = ['name',
+                  'manager',
+                  'address',
+                  'city',
+                  'state',
+                  ]
+
+
+class ChangeHMForm(forms.Form):
+    house = HouseField(queryset=House.objects.all())
+    current_HMs = House.objects.all().filter(manager_id__isnull=False).distinct()
+    new_manager = ResidentField(queryset=Resident.objects.exclude(id__in=current_HMs.values_list('manager_id', flat=True)))
+
+class ManagerMeetingForm(forms.ModelForm):
+    class Meta:
+        model = Manager_meeting
+        fields = ['title',
+                  'date',
+                  'minutes_discussed',
+                  'location',
+                  'attendee',
+                  'issues',
+                 ]
+        widgets = {
+            'date': DateInput(),
+        }
+
+class ShoppingTripForm(forms.ModelForm):
+    class Meta:
+        model = Shopping_trip
+        fields = ['date',
+        'amount',
+        'notes',
+        ]
+        
+class SupplyRequestForm(forms.ModelForm):
+    class Meta:
+        model = Supply_request
+        fields = ['date',
+                  'product',
+                  'quantity',
+                  'notes',
+                  'fulfilled',
+                  'house',
+                  'trip',
+                 ]
+
+# class NewPaymentForm(forms.ModelForm):
+#
+#     type = forms.ChoiceField(choices=TYPE_CHOICES)
+#     method = forms.ChoiceField(choices=METHOD_CHOICES, required=False)
+#     notes = forms.CharField(widget=forms.Textarea, required=False)
+#     resident = ResidentField(queryset=Resident.objects.all())
+#
+#     def get_form_class(self):
+#         if self.object.type != 'pmt':
+#             return TransactionForm
+#
+#     class Meta:
+#         model = Transaction
+#         fields = ['resident',
+#                   'date',
+#                   'type',
+#                   'amount',
+#                   'method',
+#                   'notes',
+#                   ]
+#         widgets = {
+#             'date': DateInput(),
+#         }
+#
+#
+# class DrugTestForm(forms.ModelForm):
+#     resident = ResidentField(queryset=Resident.objects.filter(discharge_date__isnull=True))
+#     other = forms.CharField(required=False)
+#
+#     class Meta:
+#         model = Drug_test
+#         fields = ['resident',
+#                   'date',
+#                   'result',
+#                   'amphetamines',
+#                   'barbiturates',
+#                   'benzodiazepines',
+#                   'cocaine',
+#                   'marijuana',
+#                   'opiates',
+#                   'phencyclidine',
+#                   ]
+#         widgets = {
+#             'date': DateInput(),
+#         }
+#
+#
+# class CheckInForm(forms.ModelForm):
+#     resident = ResidentField(queryset=Resident.objects.filter(discharge_date__isnull=True))
+#     house_managers = House.objects.all().filter(manager_id__isnull=False).distinct()
+#     manager = ResidentField(queryset=Resident.objects.filter(id__in=house_managers.values_list('manager_id', flat=True)))
+#     method = forms.ChoiceField(choices=Check_in.METHOD_CHOICES)
+#     notes = forms.CharField(widget=forms.Textarea, required=False)
+#
+#     class Meta:
+#         model = Check_in
+#         fields = ['manager',
+#                   'resident',
+#                   'date',
+#                   'method',
+#                   'notes',
+#                   ]
+#         widgets = {
+#             'date': DateInput(),
+#         }
+
+
+
+
+
+# # May be unnecessary, could add/edit/delete from console instead
+# class BedForm:
+#     x = ''
+#
+#
+# class SiteVisitForm:
+#     x = ''
+#
+#
+# class CheckInForm:
+#     x = ''
+#
+#
+# class HouseMeetingForm:
+#     x = ''
+#
+#
+
---
+++ mysite/mrtr/models.py
@@ -0,0 +1,234 @@
+from django.db import models
+from django.core.validators import MaxValueValidator
+from django.utils import timezone
+from django.db.models import Sum
+from django.db.models import CharField, Model
+
+class Resident(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.BigIntegerField(validators=[MaxValueValidator(9999999999)], null=True)
+    email = models.EmailField(max_length=62, null=True)
+    admit_date = models.DateField(default=timezone.now)
+    rent = models.IntegerField(validators=[MaxValueValidator(1000)])
+    bed = models.OneToOneField('Bed', on_delete=models.CASCADE, blank=True, null=True)
+    door_code = models.IntegerField(validators=[MaxValueValidator(9999)], null=True)
+    referral_info = models.TextField(null=True)
+    notes = models.TextField(null=True)
+    discharge_date = models.DateField(null=True)
+    submission_date = models.DateTimeField(default=timezone.now)  # automatic
+    last_update = models.DateTimeField(null=True)  # automatic
+
+    def get_absolute_url(self):
+        return '/portal/resident/%i' % self.id
+
+    def full_name(self):
+        return self.first_name + ' ' + self.last_name
+
+    def balance(self):
+        return Transaction.objects.filter(resident=self.id).aggregate(Sum('amount'))['amount__sum']
+
+
+class Transaction(models.Model):
+    date = models.DateField(default=timezone.now)
+    amount = models.DecimalField(max_digits=8, decimal_places=2)
+    # TODO Cut down on the amount of transaction types
+    TYPE_CHOICES = [
+        # # Auto apply
+        ('rnt', 'Rent charge'),
+        # ('rfd', 'Refund'),
+        # ('inc', 'Sober incentive'),
+        # ('nra', 'New rent amount adjustment'),
+
+        # Decrease balance
+        ('pmt', 'Rent payment'),
+        # ('bon', 'Bonus'),
+        # ('wrk', 'Work/reimbursement'),
+        # ('sos', "Sober support (won't pay back)"),
+
+        # Increase balance
+        ('fee', 'Fee'),
+        # ('lon', 'Loan (will pay back)'),
+
+        # Other
+        # ('fix', 'Balance fix'),
+        ('oth', 'Other adjustment (specify)')
+    ]
+    type = models.CharField(max_length=3, choices=TYPE_CHOICES, blank=True)
+    METHOD_CHOICES = [
+        ('ach', 'ACH'),
+        ('csh', 'Cash'),
+        ('cap', 'Cash App'),
+        ('chk', 'Check'),
+        ('mod', 'Money order'),
+        ('ppl', 'PayPal'),
+        ('vnm', 'Venmo'),
+        ('zel', 'Zelle'),
+        ('oth', 'Other (specify)'),
+        ('', '')
+    ]
+    method = models.CharField(max_length=3, choices=METHOD_CHOICES, blank=True)
+    notes = models.TextField(null=True)
+    resident = models.ForeignKey('Resident', on_delete=models.CASCADE)
+    submission_date = models.DateTimeField(default=timezone.now)  # automatic
+    last_update = models.DateTimeField(null=True)  # automatic
+
+
+# class Drug_test(models.Model):
+#     date = models.DateField(default=timezone.now, blank=True)
+#     RESULT_CHOICES = [
+#         ('neg', 'Negative'),
+#         ('pos', 'Positive'),
+#         ('med', 'Positive (meds)'),
+#         ('inv', 'Invalid'),
+#         ('oth', 'Other (specify)')
+#     ]
+#     result = models.CharField(max_length=3, choices=RESULT_CHOICES)
+#     amphetamines = models.BooleanField()
+#     barbiturates = models.BooleanField()
+#     benzodiazepines = models.BooleanField()
+#     cocaine = models.BooleanField()
+#     marijuana = models.BooleanField()
+#     opiates = models.BooleanField()
+#     phencyclidine = models.BooleanField()
+#     other = models.CharField(max_length=50, null=True)
+#     resident = models.ForeignKey('Resident', on_delete=models.CASCADE)
+#     submission_date = models.DateTimeField(default=timezone.now)  # automatic
+#     last_update = models.DateTimeField(null=True)  # automatic
+
+
+class House(models.Model):
+    name = models.CharField(max_length=25)
+    manager = models.ForeignKey('Resident', on_delete=models.CASCADE, db_column='manager_id', null=True)
+    address = models.CharField(max_length=50)
+    city = models.CharField(max_length=25)
+    state = models.CharField(max_length=2)
+    last_update = models.DateTimeField(null=True)  # automatic
+
+    def get_absolute_url(self):
+        return '/portal/house/%i' % self.id
+
+
+
+class Bed(models.Model):
+    name = models.CharField(max_length=7)
+    house = models.ForeignKey('House', on_delete=models.CASCADE)
+
+
+# class Check_in(models.Model):
+#     date = models.DateField(default=timezone.now)
+#     METHOD_CHOICES = [
+#         ('ip', 'In person'),
+#         ('pc', 'Phone call'),
+#         ('tx', 'Text')
+#     ]
+#     method = models.CharField(max_length=2, choices=METHOD_CHOICES)
+#     notes = models.TextField(null=True)
+#     resident = models.ForeignKey('Resident', on_delete=models.CASCADE)
+#     manager = models.ForeignKey('Resident', on_delete=models.CASCADE, db_column='manager_id', related_name='manager')  #, blank=True, null=True)  # maybe add limit_to() argument
+#     submission_date = models.DateTimeField(default=timezone.now)  # automatic
+#     last_update = models.DateTimeField(null=True)  # automatic
+
+
+class Shopping_trip(models.Model):
+    date = models.DateTimeField(default=timezone.now)  #blank=True, null=True)  # automatic
+    amount = models.DecimalField(max_digits=6, decimal_places=2)  #, blank=True, null=True)
+    notes = models.TextField(null=True)
+    
+    def get_absolute_url(self):
+        return '/portal/shopping_trip/%i' % self.id
+
+
+class Supply_request(models.Model):
+    date = models.DateTimeField(default=timezone.now)  #blank=True, null=True)  # automatic
+    PRODUCT_CHOICES = [
+        ('ppt', 'Paper towels'),
+        ('tpp', 'Toilet paper'),
+        ('ldt', 'Laundry detergent'),
+        ('diw', 'Disinfectant wipes'),
+        ('kgb', 'Garbage bags - kitchen'),
+        ('ygb', 'Garbage bags - yard'),
+        ('dso', 'Dish soap'),
+        ('dwd', 'Dishwashing detergent'),
+        ('dsp', 'Dish sponges'),
+        ('glc', 'Glass cleaner'),
+        ('apc', 'All-purpose cleaner'),
+        ('spb', 'Spray bottle'),
+        ('swp', 'Swiffer pads'),
+        ('sst', 'Sheet set - twin'),
+        ('ssf', 'Sheet set - full'),
+        ('cmt', 'Comforter - twin'),
+        ('cmf', 'Comforter - full'),
+        ('plw', 'Pillow'),
+        ('sol', 'Sign-out logs'),
+        ('vil', 'Visitor logs'),
+        ('ilr', 'Itinerary/leave request forms'),
+        ('bmd', 'B Mod Forms'),
+        ('ech', 'Meds/emergency contact sheets'),
+        ('uak', 'UA kits'),
+        ('oth', 'Other (specify')
+    ]
+    product = models.CharField(max_length=3, choices=PRODUCT_CHOICES, null=True)
+    other = models.CharField(max_length=50, null=True)
+    quantity = models.IntegerField(validators=[MaxValueValidator(10)])  # blank=True, null=True)
+    notes = models.TextField(null=True)
+    fulfilled = models.BooleanField()  #blank=True, null=True)  # automatic
+    house = models.ForeignKey('House', on_delete=models.CASCADE)  #, blank=True, null=True)
+    trip = models.ForeignKey('Shopping_trip', on_delete=models.CASCADE)  #, blank=True, null=True)  # might be unnecessary
+
+    def get_absolute_url(self):
+        return '/portal/supply_request/%i' % self.id
+
+
+class Manager_meeting(models.Model):
+    title = models.CharField(max_length=150)
+    issues = models.TextField(null=True)
+    date = models.DateField(default=timezone.now)  #blank=True, null=True)
+    location = models.CharField(max_length=50)
+    attendee = models.TextField(null=True)
+    minutes_discussed = models.BooleanField()
+    submission_date = models.DateTimeField(default=timezone.now)  #blank=True, null=True)  # automatic
+    last_update = models.DateTimeField(null=True)  # automatic
+
+    def get_absolute_url(self):
+        return '/portal/meeting/%i' % self.id
+
+# class Attendee(models.Model):
+#     meeting = models.ForeignKey('Manager_meeting', on_delete=models.CASCADE)  #, blank=True, null=True)
+#     manager = models.ForeignKey('Resident', on_delete=models.CASCADE)  #, db_column='manager_id', blank=True, null=True)  # maybe add limit_to() argument
+
+
+# class Site_visit(models.Model):
+#     date = models.DateField(default=timezone.now)  #blank=True, null=True)
+#     issues = models.TextField(null=True)
+#     manager = models.ForeignKey('Resident', on_delete=models.CASCADE, db_column='manager_id')  #, blank=True, null=True)  # maybe add limit_to() argument
+#     house = models.ForeignKey('House', on_delete=models.CASCADE)  #, blank=True, null=True)
+#     submission_date = models.DateTimeField(default=timezone.now)  #blank=True, null=True)  # automatic
+#     last_update = models.DateTimeField(null=True)  # automatic
+#
+#
+# class Manager_issue(models.Model):
+#     description = models.CharField(max_length=200)
+#     STATUS_CHOICES = [
+#         ('n', 'New'),
+#         ('o', 'Ongoing')
+#     ]
+#     status = models.CharField(max_length=1, choices=STATUS_CHOICES, null=True)
+#     expected_completion = models.DateField()  #blank=True, null=True)
+#     meeting = models.ForeignKey('Manager_meeting', on_delete=models.CASCADE)  #, blank=True, null=True)  # automatic
+#     submission_date = models.DateTimeField(default=timezone.now)  #blank=True, null=True)  # automatic
+#     last_update = models.DateTimeField(null=True)  # automatic
+
+
+# class House_meeting(models.Model):
+#     date = models.DateField(default=timezone.now)  #blank=True, null=True)
+#     issues = models.TextField(null=True),
+#     house = models.ForeignKey('House', on_delete=models.CASCADE)  #, blank=True, null=True)
+#     manager = models.ForeignKey('Resident', on_delete=models.CASCADE, db_column='manager_id')  #, blank=True, null=True)  # maybe add limit_to() argument
+#     submission_date = models.DateTimeField(default=timezone.now)  #blank=True, null=True)  # automatic
+#     last_update = models.DateTimeField(null=True)  # automatic
+#
+#
+# class Absentee(models.Model):
+#     resident = models.ForeignKey('Resident', on_delete=models.CASCADE)  #, blank=True, null=True)
+#     meeting = models.ForeignKey('House_meeting', on_delete=models.CASCADE)  #, blank=True, null=True)  # automatic
