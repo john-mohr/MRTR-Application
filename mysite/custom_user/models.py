7fa9e4ea421772e5438c7ef7671570228137485f
Deanpham1200 <deanpham1200@gmail.com>
2023-08-04 08:30:07 -0600

    TODO tj show dean singles standard

--- mysite/custom_user/models.py
+++
@@ -1,8 +0,0 @@
-from django_use_email_as_username.models import BaseUser, BaseUserManager
-from django.db import models
-from mrtr.models import Resident
-
-
-class User(BaseUser):
-    objects = BaseUserManager()
-    assoc_resident = models.ForeignKey(Resident, on_delete=models.CASCADE, blank=True, null=True)
